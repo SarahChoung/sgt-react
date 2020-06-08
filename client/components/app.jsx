e71ac61d9f8037700f5712376ced981da2403625
Sarah Choung <sarahjchoung@gmail.com>
2020-06-08 19:55:56 +0000

    Added componentDidMount method

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import React from 'react';
 
 class App extends React.Component {
@@ -6,6 +7,17 @@ class App extends React.Component {
     this.state = {
       grades: []
     };
+  }
+
+  componentDidMount() {
+    fetch('./api/grades')
+      .then(res => res.json())
+      .then(grades => {
+        console.log(grades);
+        this.setState({
+          grades: grades
+        });
+      });
   }
 
   render() {
